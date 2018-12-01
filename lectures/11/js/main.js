4fa2e700b698811873cb0b80810fb9b741c31842
Zain1123 <zain.ali3242@yahoo.com>
2018-12-01 23:13:52 +0500

    Todo class activity done!

--- lectures/11/js/main.js
+++ lectures/11/js/main.js
@@ -8,9 +8,11 @@ function add() {
     var task = document.getElementById("task").value;
     todos.push(task);
     document.getElementById('todos').innerText = todos;
-}*/
-
-
+}
+Description:
+ // Data we enter in input box,store in task variable.
+ // With the push command, we put data iin todos array.
+*/
 //============================================
 //-----------  STEP - 2 ----------------------
 //============================================
@@ -34,14 +36,16 @@ function show() {
         ul.appendChild(li);
     }
     document.getElementById('todos').appendChild(ul);
-}*/
-
-
+}
+Decription:
+// In add funtion,get input and push it in todos array then remove data from input box.
+//at the of this funtion, its call function show(), in this,it make a list of ul and print all array of
+// todos with empty array
+// at the is use .appendChild for make li to child of ul.
+*/
 //============================================
 //-----------  STEP - 3 ----------------------
 //============================================
-
-
 /*
 function getTodos() {
     var todos = [];
@@ -65,8 +69,6 @@ function add() {
     localStorage.setItem('todo',JSON.stringify(todos));
     show();
 }
-
-
 function show() {
     document.getElementById('todos').innerText = '';
     var todos = getTodos();
@@ -76,24 +78,27 @@ function show() {
         var li = document.createElement('li');
         li.innerHTML  = '<li>' + todos[i].task + '</li>' +
             '<button class="btn btn-danger">' +
-            '<i class="fas fa-trash-o"></i> ' +
-            '<span class="d-none d-sm-inline"> Delete </span> </button>';
+            '<i class="fas fa-trash-o"></i>' +
+            '<span class="d-none d-sm-inline">Delete</span></button>';
         li.classList.add("list-group-item");
         ul.appendChild(li);
     }
     document.getElementById('todos').appendChild(ul);
 }
 show();
+Description:
+ In gettodos function,its make todos array and get some data from localstorage.getitem() and JSON.parse() with this
+ command save all data in array and return array.
+ In add function, save input data in task variable and call gettodoes function, push data from task in array
+ store data using JSON.stringify() function.
+ In show() funution, make a list of ul and make child of li.
 */
-
-
 
 //============================================
 //-----------  STEP - 4 ----------------------
 //============================================
-
-
-/*function getTodos() {
+/*
+function getTodos() {
     var todos = [];
     var todos_str = localStorage.getItem('todo');
     if(todos_str !== null)
@@ -162,8 +167,14 @@ function isDone(e) {
     localStorage.setItem('todo',JSON.stringify(todos));
     show();
 }
-show();*/
+show();
+Description:
 
+/*In the step 4, some new function like remove ,isDone and add some new thing in show(). In Show() function, give
+ delete funtion on click property with the help of .addEventListener.
+ In remove function(), its get id of button by use getattribute() and use .splice() for remove data.
+ In function isDone(e),
+*/
 
 
 
@@ -180,8 +191,89 @@ CLASS ACTIVITY (HOME WORK) :
 HINT: use text-decoration:line-through; property of CSS
 
 2- Enable Editing todos in text field to update text
-
-
-
 */
+function getTodos() {
+    var todos = [];
+    var todos_str = localStorage.getItem('todo');
+    if(todos_str !== null)
+        todos = JSON.parse(todos_str);
+    return todos;
+}
+
+function add() {
+    var task = document.getElementById('task').value;
+    if(task.trim() == ''){
+        document.getElementById('message').style.display = 'block';
+        return false;
+    } else {
+        document.getElementById('message').style.display = 'none';
+    }
+    var todos = getTodos();
+    todos.push({task: task, isDone: false});
+    document.getElementById('task').value = '';
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = getTodos();
+    todos.splice(id,1);
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+function change(){
+    var id = this.getAttribute('id');
+    var todos = getTodos();
+    todos[id] = document.getElementById('task').value;
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+
+function show() {
+    document.getElementById('todos').innerText = '';
+    var todos = getTodos();
+    var ul = document.createElement('ul');
+    ul.classList.add("list-group");
+    for(var i=0; i<todos.length; i++){
+        var li = document.createElement('li');
+        li.innerHTML  = '<li>' + todos[i].task + '</li>' +'<button class="btn btn-primary" id="' + i + '">' +
+            '<i class="fa fa-trash-o"></i> ' +
+            '<span class="d-none d-sm-inline"> Edit</span> </button>'+
+            '<button class="btn btn-danger" id="' + i + '">' +
+            '<i class="fa fa-trash-o"></i> ' +
+            '<span class="d-none d-sm-inline"> Delete </span> </button>';
+        li.classList.add("list-group-item");
+        if(todos[i].isDone)
+            li.classList.add("done");
+        ul.appendChild(li);
+    }
+    document.getElementById('todos').appendChild(ul);
+    var buttons = document.getElementsByClassName('btn-danger');
+    for(var i=0; i<buttons.length; i++){
+        buttons[i].addEventListener('click',remove);
+    }
+    var buttons1 = document.getElementsByClassName('btn-primary');
+    for(var j=0; j<buttons.length; j++){
+        buttons1[j].addEventListener('click',change);
+    }
+}
+
+function isDone(e) {
+    var todos = getTodos();
+    if(todos[e.target.id].isDone) {
+        e.target.classList.add('done');
+        todos[e.target.id].isDone = false;
+    }
+    else{
+        e.target.classList.remove('done');
+        todos[e.target.id].isDone = true;
+    }
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+}
+show();
 
