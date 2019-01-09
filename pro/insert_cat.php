e8670ad109a6eabd9a1d3eab5f324e010ccb6097
Zain1123 <zain.ali3242@yahoo.com>
2019-01-09 18:20:41 +0500

    Quiz 5 done!

--- pro/insert_cat.php
+++ pro/insert_cat.php
@@ -1,7 +1,7 @@
 <?php
 include "server/db_connection.php";
-if(isset($_POST['insert_cat'])){
-    $title = $_POST['pro_title'];
+if(isset($_GET['insert_cat'])){
+    $title = $_GET['pro_title'];
     $input = "insert into categories (cat_title) values ('$title')";
     mysqli_query($con,$input);
     if(!$input){
@@ -32,7 +32,7 @@ if(isset($_POST['insert_cat'])){
 <body>
 <div class="container">
     <h1 class="text-center my-5"><i class="fas fa-plus fa-md"></i> <span class="d-none d-sm-inline"> Add New </span> Category </h1>
-    <form action="insert_cat.php" method="post">
+    <form action="insert_cat.php" method="get">
         <div class="row">
             <div style="margin-top: 3px" class="col ">
                 <label for="pro_title" class="float-md-right d-none d-md-inline d-lg-inline d-sm-inline"> <span class="d-sm-none d-md-inline"> Product </span> Category:</label>
